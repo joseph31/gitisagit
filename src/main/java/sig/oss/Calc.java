776f45ee67a82f8133737c81feac4da2d46b8684
joseph31 <jinux31@gmail.com>
2016-08-23 23:42:11 -0700

    add mul func

--- src/main/java/sig/oss/Calc.java
+++ src/main/java/sig/oss/Calc.java
@@ -1,6 +1,15 @@
 public class Calc {
+	private int r=0;
+	
 	public int add(int x, int y){ 
 		return x+y; 
 	}
+	public int sub(int x, int y) {
+		return x-y;
+	}
+	public int mul(int x, int y) {
+		return x*y;
+	}
+	
 }
 
